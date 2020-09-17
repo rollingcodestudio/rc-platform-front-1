46c89bfa410d42df715e3eef4206cefa533a5ec3
Nico <nicogonzalez2008@gmail.com>
2020-09-17 12:41:40 -0300

    add regpass sin logica

--- src/resetpass/Regpassword.js
+++ src/resetpass/Regpassword.js
@@ -1,22 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { auth } from "../firebase";
 import './regpassword.css';
 
+
 const Regpassword = () => {
+
+    const [changepassword, setChangepassword] = useState({
+        password: "",
+        confirmpassword: ""
+    });
+
+    const handleChange = e => {
+        setChangepassword({
+            ...changepassword,
+            [e.target.name]: e.target.value,
+        });
+        console.log(changepassword)
+    }
+
+    const handleRegpass = async (e , code) => {
+
+        e.preventDefault();
+
+        try {
+            const respregpass = await auth.sendPasswordResetEmail( code , changepassword.password)
+            console.log(respregpass)
+        } catch (error) {
+    
+            console.log(error)
+        }
+    }
+
     return (
         <div className="containerregpass">
             <div className="colorcont">
                 <div className="centerpage">
                     <h6>Ingrese su nueva contraseña.</h6>
-                    <form>
+                    <form onSubmit={handleRegpass}>
                         <div className="form-group mt-4">
                             <label>Contraseña:</label>
-                            <input type="password" className="form-control" autoComplete="off" />
+                            <input type="password" name="password" value={changepassword.password} onChange={handleChange} id="password" className="form-control" autoComplete="off" />
                         </div>
                         <div className="form-group mt-4">
                             <label>Confirmar contraseña:</label>
-                            <input type="password" className="form-control" autoComplete="off" />
+                            <input type="password" name="confirmpassword" value={changepassword.confirmpassword} onChange={handleChange} id="confirmpassword" className="form-control" autoComplete="off" />
                         </div>
-                        <button type="submit" className="mt-4 btn btn-outline-light btn-block">Restablecer contraseña</button>
+                        <button type="submit" onClick={handleRegpass} className="mt-4 btn btn-outline-light btn-block">Restablecer contraseña</button>
                     </form>
                 </div>
             </div>
